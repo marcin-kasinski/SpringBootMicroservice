30a27ede907952a6d55910521e89aa1c3ecc0d7b
marcin <marcin.kasinski@gmail.com>
2018-08-20 10:01:19 +0200

    add http debug

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /etc/init.d/filebeat start
 
-java -jar -Dspring.profiles.active=$SPRING_PROFILE SpringBootMicroservice-0.0.1-SNAPSHOT.jar
+java -jar -Dspring.profiles.active=$SPRING_PROFILE -Dlogging.level.org.apache.http=DEBUG SpringBootMicroservice-0.0.1-SNAPSHOT.jar
