d7ca22235585a62dd5d0d6b96cfc9ca18e8eda57
marcin <marcin.kasinski@gmail.com>
2018-11-14 19:58:42 +0100

    add Serializable to mk.model.User

--- src/main/java/mk/model/User.java
+++ src/main/java/mk/model/User.java
@@ -1,6 +1,8 @@
 package mk.model;
 
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,7 +14,7 @@ import javax.persistence.Table;
 
 @Entity // This tells Hibernate to make a table out of this class
 @Table(name = "users")
-public class User {
+public class User implements Serializable{
 	
 /*	
 	@Bean
@@ -27,7 +29,12 @@ public class User {
 	}
 	*/
 	
-    @Id
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 
